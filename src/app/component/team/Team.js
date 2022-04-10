28956071e06ea7cc6121c7f447484e4402254747
Jānis Kļaviņš <jklavins14@gmail.com>
2022-04-10 20:56:43 +0300

    Styling fixes

--- src/app/component/team/Team.js
+++ src/app/component/team/Team.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Team.scss';
 const Team = ({ data, header }) => {
-  //url, image, name, role
+  //url, image, name, role - data is an array of object [{}, {}, {}...]
 
   const renderItems = (memberObject, idx) => {
     return (
       <div className="Team-Container" key={idx}>
         <a href={memberObject.url}>
-          <img src={memberObject.image} alt={memberObject.name} />
+          <img src={memberObject.image} alt={memberObject.name} target="_blank"/>
         </a>
-        <p>{memberObject.name}</p>
-        <p>{memberObject.role}</p>
+        <p className='Team-Name'>{memberObject.name}</p>
+        <p className='Team-Role'>{memberObject.role}</p>
       </div>
     );
   };
