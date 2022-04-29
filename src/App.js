a3012bb4dcf5b324061364e580a33675c9707850
Jānis Kļaviņš <jklavins14@gmail.com>
2022-04-29 13:09:06 +0300

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
-import MainPage from './app/route/mainPage/MainPage';
+import Loader from './app/component/Loader/Loader';
+import React, { Suspense } from 'react';
+
+const MainPage = React.lazy(() => import('./app/route/mainPage/MainPage'));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<Loader />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
